540f513fa23301a62c18c55f7d58fd51e01f81d6
abecderic <abecderisch@web.de>
2017-04-16 09:53:20 +0200

    Add Wrench

---
+++ src/main/java/com/abecderic/mnu/item/ItemWrench.java
@@ -0,0 +1,28 @@
+package com.abecderic.mnu.item;
+
+import com.abecderic.mnu.MNU;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemWrench extends Item
+{
+    public ItemWrench()
+    {
+        setCreativeTab(MNU.TAB);
+        setMaxStackSize(1);
+        setUnlocalizedName(MNUItems.WRENCH);
+        setHasSubtypes(true);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return "item." + MNU.MODID + ":" + MNUItems.WRENCH;
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack stack)
+    {
+        return getUnlocalizedName();
+    }
+}
--- src/main/java/com/abecderic/mnu/item/MNUItems.java
+++ src/main/java/com/abecderic/mnu/item/MNUItems.java
@@ -6,16 +6,24 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class MNUItems
 {
     public static final String DM_SCANNER = "dm_scanner";
     public static Item dmScanner;
 
+    public static final String WRENCH = "wrench";
+    public static Item wrench;
+
     public static void registerItems()
     {
         dmScanner = new ItemDMScanner();
         registerItem(dmScanner, DM_SCANNER);
+
+        wrench = new ItemWrench();
+        registerItem(wrench, WRENCH);
+        OreDictionary.registerOre("itemWrench", wrench);
     }
 
     private static void registerItem(Item item, String name)
@@ -27,6 +35,15 @@ public class MNUItems
     public static void registerModels()
     {
         registerModelWithMeta(dmScanner, DM_SCANNER, 7);
+        registerModel(wrench, WRENCH);
+    }
+
+    private static void registerModel(Item item, String name)
+    {
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0,
+                new ModelResourceLocation(MNU.MODID + ":" + name, "inventory"));
+        ModelLoader.setCustomModelResourceLocation(item, 0,
+                new ModelResourceLocation(MNU.MODID + ":" + name, "inventory"));
     }
 
     private static void registerModelWithMeta(Item item, String name, int amount)
